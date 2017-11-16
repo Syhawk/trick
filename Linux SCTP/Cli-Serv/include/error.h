abff6d1dba64e9699dcf05234c018e50ecebce98
Syhawk <805782175@qq.com>
2017-11-16 21:30:47 +0800

    tmp

---
+++ Linux SCTP/Cli-Serv/include/error.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_ERROR_H
+#define INCLUDE_ERROR_H
+
+#include "unp.h"
+
+#include <stdarg.h>
+#include <syslog.h>
+
+void err_sys(const char* fmt, ...);
+void err_doit(int errnoflag, int level, const char* fmt, va_list ap);
+void err_quit(const char* fmt, ...);
+void err_ret(const char* fmt, ...);
+
+#endif	// INCLUDE_ERROR_H
